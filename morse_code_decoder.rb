f176c2e26f0ab11a044cb3f2898787fb0c5f06e6
HENRY-KC <henrykc24@yahoo.com>
2021-11-03 19:10:11 +0100

    Add the third function

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -1,19 +1,42 @@
-@morse = {
-    ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E", "..-." => "F", "--." => "G", "...." => "H", ".." => "I", 
-    ".---" => "J", "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",  ".--." => "P", "--.-" => "Q", ".-." => "R",
-    "..." => "S", "-" => "T", "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y", "--.." => "Z" }
-def decode_char str
-        return morse[str]
-    end
 
-# Create a method to decode an entire word in Morse code, takes a string parameter, and return the string representation.
-# Every character in a word will be separated by a single space (e.g. decode_word("-- -.--") returns "MY").
+def decode_char str
+    morse = {
+        ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E", "..-." => "F", "--." => "G", "...." => "H", ".." => "I", 
+        ".---" => "J", "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",  ".--." => "P", "--.-" => "Q", ".-." => "R",
+        "..." => "S", "-" => "T", "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y", "--.." => "Z" }
+    return morse[str]
+end
+
 def decode_word str
     str_ar = str.split(" ") 
     result = ""     
     str_ar.each do |n| 
-        result += @morse[n]
+        result += decode_char n
     end
     return result
 end
-decode_word("-- -.--")
+
+def decode str
+    temp_str = ''
+    (str.length).times do |i|
+        if str[i] != '.' && str[i] != '-' && str[i] != ' ' 
+            temp_str += '*'
+            next
+        end
+        if (str[i] === ' ' && str[i - 1] === ' ' && str[i + 1] === ' ')
+            temp_str += '*';
+        else 
+            temp_str += str[i];
+        end
+    end
+    str_ar = temp_str.split(' ')
+    result = ''
+    str_ar.each do |one|
+        if one === '*'
+            result += ' '
+        else
+            result += decode_char(one)
+        end
+    end
+    return result
+end

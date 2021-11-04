4db611653cbb5ad0273dfd46e8fd353540c66329
ali abbani <aliox@ahlam.com>
2021-11-04 09:59:24 +0200

    Fix the third methods and the linter errors

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -1,46 +1,35 @@
-def decode_char str
-    morse = {
-        ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E", "..-." => "F", "--." => "G", "...." => "H", ".." => "I", 
-        ".---" => "J", "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",  ".--." => "P", "--.-" => "Q", ".-." => "R",
-        "..." => "S", "-" => "T", "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y", "--.." => "Z" }
-    return morse[str]
+# frozen_string_literal: true
+
+def decode_char(str)
+  morse = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F',
+    '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+    '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
+  }
+  morse[str]
 end
 
-def decode_word str
-    str_ar = str.split(" ") 
-    result = ""     
-    str_ar.each do |n| 
-        result += decode_char n
-    end
-    return result
+def decode_word(str)
+  str_ar = str.split(' ')
+  result = ''
+  str_ar.each do |n|
+    result += decode_char n
+  end
+  result
 end
 
-def decode str
-    temp_str = ''
-    (str.length).times do |i|
-        if str[i] != '.' && str[i] != '-' && str[i] != ' ' 
-            temp_str += '*'
-            next
-        end
-        if (str[i] === ' ' && str[i - 1] === ' ' && str[i + 1] === ' ')
-            temp_str += '*';
-        else 
-            temp_str += str[i];
-        end
-    end
-    str_ar = temp_str.split(' ')
-    result = ''
-    str_ar.each do |one|
-        if one === '*'
-            result += ' '
-        else
-            result += decode_char(one)
-        end
-    end
-    return result
+def decode(str)
+  str_ar = str.split('  ')
+  result = []
+  str_ar.each do |n|
+    result.push(decode_word(n))
+  end
+  result.join(' ')
 end
 
-decode_char(".-")
-decode_word("-- -.--")
-decode("-- -.--   -. .- -- .")
-decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...")
+decode_char('.-')
+decode_word('-- -.--')
+decode('-- -.--   -. .- -- .')
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
